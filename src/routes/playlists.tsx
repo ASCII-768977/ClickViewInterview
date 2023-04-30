f99545778dc79399faf6e24e2c87c99e0429e3af
Forrest Lin <forrest.lin.developer@gmail.com>
2023-04-30 11:26:06 +1000

    task1

--- src/routes/playlists.tsx
+++ src/routes/playlists.tsx
@@ -1,7 +1,32 @@
+import { PlaylistItem } from '../components/playlist-item';
+import { useState, useEffect } from 'react';
+
+import { Playlist } from '../interfaces/playlist';
+
 export function Playlists() {
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+  useEffect(() => {
+    let isCancelled = false;
+    fetch('./playlists.json')
+      .then((response) => response.json())
+      .then((data) => {
+        if (!isCancelled) {
+          setPlaylists(data);
+        }
+      });
+
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+
   return (
     <main>
       <h1>Playlists route</h1>
+      {playlists.map((playlist) => (
+        <PlaylistItem key={playlist.id} playlist={playlist} />
+      ))}
     </main>
   );
 }
